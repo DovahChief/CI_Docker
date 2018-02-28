9fcff262d8c0f743836bd65355147f15947949ad
DovahChief <pepelimonta@gmail.com>
2018-02-28 13:43:14 -0600

    Docker a Script Bash

--- dockerScript.sh
+++ dockerScript.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Crea la imagen de Docker
+docker build -f Dockerfile -t test  .
+
 #docker save -o ./image.tar project-dockerImage
 # docker load -i 
 echo "COMANDO PARA PASAR IMAGEN DE DOCKER"
